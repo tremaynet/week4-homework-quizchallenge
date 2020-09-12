472054c0280286bf9cc5323f6376cca7063aa279
Tremayne Terry <tremaynet@gmail.com>
2020-09-12 18:09:07 -0400

    inital js file creation to include defining variables

---
+++ assets/js/quizlogic.js
@@ -0,0 +1,13 @@
+// variables to keep track of quiz state
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
+
+// variables to reference DOM elements
+var questionsEl = document.getElementById("questions");
+var timerEl = document.getElementById("time");
+var choicesEl = document.getElementById("choices");
+var submitBtn = document.getElementById("submit");
+var startBtn = document.getElementById("start");
+var initialsEl = document.getElementById("initials");
+var feedbackEl = document.getElementById("feedback");
