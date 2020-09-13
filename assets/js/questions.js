24cd5ca45b063e46febbbf7fc912579c7d7012cd
Tremayne Terry <tremaynet@gmail.com>
2020-09-13 17:10:23 -0400

    incorporating quizlogic

---
+++ assets/js/questions.js
@@ -0,0 +1,55 @@
+var jsQuestions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Wha a user views a page containing a JavaScript program, which machine actuall executes the script?",
+        choices: ["The User's machine running a Web browser", "The Web server", "A central machine deep within the ISP", "None of the above"],
+        answer: "The User's machine running a Web browser"
+    },
+    {
+        title: "Arrays in JavaScript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is?",
+        choices: ["JavaScript", "terminal / Bash", "for loops", "consol.log"],
+        answer: "consol.log"
+    },
+    {
+        title: "_______JavaScript statements embedded in an HTML page can respond to user events such as mouse-clicks, form input, and page navigation",
+        choices: ["Client-side", "Server-side", "Local", "Native"],
+        answer: "Client-side"
+    }
+];
+
+var jqueryQuestions = [
+    {
+        title: "Can you assign a anonymous function to a variable?",
+        choices: ["True", "False"],
+        answer: "True"
+    },
+    {
+        title: "Which built-in method returns the character at the specified index?",
+        choices: ["characterAt()", "getCharAt()", "chartAt()", "None of the above"],
+        answer: "charAt()"
+    },
+    {
+        title: "Which of the following jQuery method checks the current selection against an expression?",
+        choices: ["getls( selector )", "is( selector )", "checkls( selector )", "None of the above"],
+        answer: "is( selector )"
+    },
+    {
+        title: "Which of the following jQuery method gets the style property of an element?",
+        choices: ["getClass( name )", "getStyle( name )", "css( name )", "None of the above"],
+        answer: "css( name )"
+    }
+]
